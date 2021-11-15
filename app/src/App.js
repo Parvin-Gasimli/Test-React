5a757d9f9d0f0e906de107309ba98a9fe2f16cb4
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-11-15 16:22:55 +0300

    Test React Start

---
+++ app/src/App.js
@@ -0,0 +1,22 @@
+
+
+
+function App() {
+  return (
+    <nav>
+      <h1>
+        LOGO
+      </h1>
+      <ul>
+        <li>Main Page</li>
+        <li>Project</li>
+        <li>About Us</li>
+        <li> Product</li>
+      
+      </ul>
+
+    </nav>
+  );
+}
+
+export default App;
