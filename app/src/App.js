5df21a35cc4753b9c7544b89fa60f1aedc7e2cc9
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-11-15 18:03:48 +0300

    Hooks

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,44 @@
+import React,{useState,useEffect} from 'react'
 
-
-
+import './index.css'
 function App() {
-  return (
-    <>
-    <nav>
-      <h1>
-        LOGO
-      </h1>
-      <ul>
-        <li>Main Page</li>
-        <li>Project</li>
-        <li>About Us</li>
-        <li> Contact</li>
-      
-      </ul>
+// const [count ,SetCount]=useState(2);
 
-    </nav>
-    <section>
+// function increase(){
+//   SetCount(oldvalue=>oldvalue+1)
+// }
+// function descrease(){
+//   SetCount(oldvalue=>oldvalue-1)
+// }
+  
+//   return (
+//     <div>
+//     <button onClick={descrease}>-</button>
+//     <span>{count}</span>
+//     <button onClick={increase}>+</button>
+//     </div>
+//   );
+// 
 
-    </section>
-    </>
-  );
+const [counts ,SetCount]=useState(0);
+const [button,setButton]=useState('user')
+useEffect(()=>{
+  console.log(`basilan button ${button}`)
+},[counts])
+
+return(
+  <div>
+    <div>
+      <button onClick={()=>setButton("User")}>User</button>
+      <button onClick={()=>setButton("Comments")}>Comments</button>
+      <button onClick={()=>setButton("Post")}> Posts</button>
+      <h1> {button}</h1>
+    </div>
+    <button onClick={()=>SetCount(counts+1)}> Count</button>
+    <h1> </h1>
+  </div>
+)
+
 }
 
 export default App;
